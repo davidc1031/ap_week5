45b32d5bdecc97bd3f4288c66239d7e93f9ee7b1
davidc1031 <cchuy709@gmail.com>
2025-11-06 18:51:34 +0000

    manipulating words

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -42,7 +42,11 @@ extract_name = print(famous_quote[83: ])
 # Manipulating Words:
 # Given the string info = "Python is fun. Fun is good. Good is subjective.",
 # a. Extract the word 'subjective' without knowing its exact position.
+python = "Python is fun. Fun is good. Good is subjective."
+subject = print(python.find ('subjective') )
+extract_word = print(python[36: ])
 # b. Extract every third word.
+
 # c. Reverse the positions of the words, but keep the characters in each word in the same order.
 
 # Problem Set 3: String Methods
